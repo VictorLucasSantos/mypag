52913e53c94850e6d507cb15c996339b94eb57d0
Victor Lucas Santos Gonzaga <victorlucas.santos124@gmail.com>
2024-03-07 12:54:48 -0400

    Adiciona arquivo Admin Django

--- mypag/mypag/urls.py
+++ mypag/mypag/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path('home', include('blog.urls'))
+    path('admin/', admin.site.urls), path('home', include('blog.urls'))
 ]
